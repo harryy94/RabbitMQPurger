f51989fc37327f7262674c492341e85f881a71e6
Harrison Young <Harrison.Young@mab.org.uk>
2020-12-16 17:19:02 +0000

    Add project files.

---
+++ RabbitMQPurger/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQPurger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rabbitMqClient = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                HostName = "localhost"
+            };
+
+            var connection = rabbitMqClient.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+            string jsonResponse;
+            using (var webClient = new WebClient())
+            {
+                var request = $"http://{rabbitMqClient.HostName}:15672/api/queues";
+
+                webClient.Credentials = new NetworkCredential(rabbitMqClient.UserName, rabbitMqClient.Password);
+                jsonResponse = webClient.DownloadString(request);
+            }
+
+            var queues = JsonConvert.DeserializeObject<List<QueueResponse>>(jsonResponse);
+
+            foreach (var queue in queues)
+            {
+                Console.WriteLine("Purging " + queue.Name);
+                channel.QueuePurge(queue.Name);
+            }
+        }
+
+        public class QueueResponse
+        {
+            public string Name { get; set; }
+        }
+    }
+}
